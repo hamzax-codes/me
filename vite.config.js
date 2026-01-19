d5d123c258b32ff7433c9d79da81d8415c2ab4b6
M Hamza <ha243695@gmail.com>
2026-01-20 00:43:53 +0500

    Fix: Update base path to /me/

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Replace 'portfolio' with your GitHub repo name
+  base: '/me/', // Matches GitHub repo name
 })
